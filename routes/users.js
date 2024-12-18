90b27428ac96eb4b6d5446e6fb7ad920df78239e
yamine59 <yamineizi22@gmail.com>
2024-12-18 19:35:47 +0100

    tout

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,33 @@ const db = mysql.createConnection({
 
 
 
-
+/**
+ * @swagger
+ * /register:
+ *    post:
+ *      summary: Enregistrer un utilisateur
+ *      responses:
+ *        201:
+ *          description: Utilisateur créé avec succès
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Utilisateur créé"
+ *        500:
+ *          description: Erreur serveur
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  error:
+ *                    type: string
+ *                    example: "Erreur serveur lors de l'insertion de l'utilisateur"
+ */
 router.post('/register', async (req, res) => {
     const { username, password, name, firstname } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -31,6 +57,63 @@ router.post('/register', async (req, res) => {
     })
 })
 
+
+
+
+/**
+ * @swagger
+ * /login:
+ *   post:  
+ *     summary: Connexion utilisateur
+ *     description: Authentifie un utilisateur et retourne un token JWT si la connexion réussit.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "yamine"
+ *               password:
+ *                 type: string
+ *                 example: "password123"
+ *     responses:
+ *       200:
+ *         description: Utilisateur connecté avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Utilisateur connecté"
+ *                 token:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR..."
+ *       401:
+ *         description: Échec de l'authentification
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Mot de passe incorrect"
+ *       500:
+ *         description: Erreur serveur
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Erreur serveur"
+ */
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
@@ -50,6 +133,9 @@ router.post('/login', async (req, res) => {
                 id: user.id,
                 username: user.username,
                 role: user.role,
+                name: user.name,
+                firstname: user.firstname
+
             },
             process.env.TOKEN_SECRET,  // Utilisez la variable d'environnement
             { expiresIn: '1h' }
@@ -80,7 +166,67 @@ function authenticateToken(req, res, next) {
     })
 }
 
-router.get('/profile', authenticateToken, (req, res) => {
+
+/**
+ * @swagger
+ * /profil:
+ *   get:
+ *     summary: Récupérer le profil de l'utilisateur
+ *     description: Retourne le profil de l'utilisateur authentifié.
+ *     security:
+ *       - bearerAuth: []   # Indique qu'un token JWT est requis pour l'accès
+ *     responses:
+ *       200:
+ *         description: Profil de l'utilisateur récupéré avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "profil de l utilisateur"
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 25
+ *                     username:
+ *                       type: string
+ *                       example: "yamine"
+ *                     role:
+ *                       type: string
+ *                       example: "admin"
+ *                     name:
+ *                       type: string
+ *                       example: "Yamine"
+ *                     firstname:
+ *                       type: string
+ *                       example: "Doe"
+ *       401:
+ *         description: Utilisateur non authentifié
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Accès refusé. Token manquant ou invalide."
+ *       500:
+ *         description: Erreur serveur
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Erreur serveur"
+ */
+
+router.get('/profil', authenticateToken, (req, res) => {
     res.json({ message: "profil de l utilisateur", user: req.user })
    
 })
@@ -88,13 +234,62 @@ router.get('/profile', authenticateToken, (req, res) => {
 
 
 
+
+
+
+
+
+
+/**
+ * @swagger
+ * /users:
+ *    get:
+ *      summary: recuperer les utilisateur
+ *      responses:
+ *        201:
+ *          description: liste des utilisateur
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                      id:
+ *                          type: interger
+ *                          example: 25
+ *                      username:
+ *                          type: string
+ *                          example: "yaminnee"
+ *                      firstname:
+ *                          type: string
+ *                          example: "yaminnee"
+ *                      name:
+ *                          type: string
+ *                          example: "benkouider"
+ *                      role:
+ *                          type: string
+ *                          example: "admin"      
+ *                      created_at:
+ *                          type: timestamp
+ *                          example: "2024-08-27T12:09:49.000Z"                  
+ */
 router.get('/', (req, res) => {
     const sql = 'SELECT * FROM users';
     db.query(sql, (err, results) => {
         if (err) {
             return res.status(500).send(err);
         }
-        res.status(200).json(results);
+        const filtreuser = results.map(user => ({
+            id : user.id,
+            username: user.username,
+            name: user.name,
+            firstname: user.firstname,
+            role: user.role,
+            created_at: user.created_at
+            
+        }))
+        res.status(200).json(filtreuser);
     });
 })
 
